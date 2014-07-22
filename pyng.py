3b48ff9ac657659d46f9fa0ac3f166fb6e61cd8c
Tyler Krupicka <tylerkrupicka@gmail.com>
2014-07-22 14:36:11 -0400

    Setup config and python file

---
+++ pyng.py
@@ -0,0 +1,16 @@
+import re as regex
+
+class Pyng:
+
+    def __init__(self):
+        #set variables
+        self.config = "config.txt"
+
+    def main(self):
+        print("hello world")
+
+if __name__ == '__main__':
+    Pyng().main()    
+
+
+    
